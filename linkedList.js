36b03de574af38a903bebe47bcabce14e128e81d
Ebrahiem Francis <ebrahiemfrancis741@gmail.com>
2025-01-30 06:54:04 +0200

    Add property declarations for the createLinkedList() factory function

---
+++ linkedList.js
@@ -0,0 +1,18 @@
+import { createNode } from "./node";
+
+function createLinkedList() {
+  return {
+    head: null,
+    tail: null,
+    size: 0,
+    append: function () {},
+    prepend: function () {},
+    at: function (index) {},
+    pop: function () {},
+    contains: function (value) {},
+    find: function (value) {},
+    toString: function () {},
+    insertAt: function (value, index) {},
+    removeAt: function (index) {},
+  };
+}
