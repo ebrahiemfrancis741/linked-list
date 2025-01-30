7032ac4eea86f3d395a79bd01bdd3b51a84e74f2
Ebrahiem Francis <ebrahiemfrancis741@gmail.com>
2025-01-30 21:12:38 +0200

    Add definition for toString() method

--- linkedList.js
+++ linkedList.js
@@ -1,4 +1,4 @@
-import { createNode } from "./node";
+import { createNode } from "./node.js";
 
 function createLinkedList() {
   return {
@@ -9,18 +9,42 @@ function createLinkedList() {
       if (this.size == 0) {
         this.head = createNode(value);
         this.tail = this.head;
-      }
-      else{
+      } else {
         this.tail.next = createNode(value);
+        this.tail = this.tail.next;
       }
+      this.size++;
     },
     prepend: function (value) {},
     at: function (index) {},
     pop: function () {},
     contains: function (value) {},
     find: function (value) {},
-    toString: function () {},
+    toString: function () {
+      let node = this.head;
+      if (node == null) {
+        console.log("( null )");
+        return;
+      }
+      let string = "";
+      while (node != null) {
+        string += `( ${node.value} ) -> `;
+        node = node.next;
+      }
+      string += "null";
+      console.log(string);
+    },
     insertAt: function (value, index) {},
     removeAt: function (index) {},
   };
 }
+
+let list = createLinkedList();
+list.toString();
+list.append("apple");
+list.append("cat");
+list.append("parrot");
+list.append("hamster");
+list.append("snake");
+list.append("turtle");
+list.toString();
